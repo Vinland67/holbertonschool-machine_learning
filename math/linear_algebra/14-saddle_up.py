ff44d1e034dc3780ee5e324449bfb2792b6d83cb
Vinland67 <13626@holbertonstudents.com>
2026-04-21 21:30:21 +0000

    Add task 14: NumPy matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Performs matrix multiplication using NumPy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Function that performs matrix multiplication"""
+    return np.matmul(mat1, mat2)
