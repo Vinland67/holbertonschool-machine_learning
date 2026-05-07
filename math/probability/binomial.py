c8bd61ec36a94355b6971eaf300ae02904c58ea0
Vinland67 <13626@holbertonstudents.com>
2026-05-07 19:51:17 +0000

    Add task 11: Binomial PMF

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -19,17 +19,34 @@ class Binomial:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
-            # Mean (mu) and Variance (sigma^2)
             mean = sum(data) / len(data)
             sum_diff_sq = sum((x - mean) ** 2 for x in data)
             variance = sum_diff_sq / len(data)
-
-            # Estimate p first, then n
-            # p = 1 - (variance / mean)
             p_estimated = 1 - (variance / mean)
             self.n = int(round(mean / p_estimated))
-
-            # Recalculate p based on the rounded n
             self.p = float(mean / self.n)
 
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        # Faktorial funksiyası (internal helper)
+        def fact(n):
+            res = 1
+            for i in range(1, n + 1):
+                res *= i
+            return res
+
+        # Kombinasiya: n! / (k! * (n-k)!)
+        n_fact = fact(self.n)
+        k_fact = fact(k)
+        nk_fact = fact(self.n - k)
+        combination = n_fact / (k_fact * nk_fact)
+
+        # PMF = combination * (p^k) * ((1-p)^(n-k))
+        pmf_val = combination * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+        return pmf_val
