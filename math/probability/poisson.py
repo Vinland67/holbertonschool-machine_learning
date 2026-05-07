aa1fe32f28394054931dc007957481fe35092d68
Vinland67 <13626@holbertonstudents.com>
2026-05-07 19:44:27 +0000

    Add task 1: Poisson PMF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -17,3 +17,25 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes"""
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        # e sabiti (2.7182818285)
+        e = 2.7182818285
+
+        # k! (faktorial) hesablamaq
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        # P(k) = (e^-lambda * lambda^k) / k!
+        # e^-lambda = 1 / (e^lambda)
+        pmf_val = ((e ** -self.lambtha) * (self.lambtha ** k)) / factorial
+
+        return pmf_val
