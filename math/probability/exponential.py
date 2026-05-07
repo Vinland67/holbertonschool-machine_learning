ce9319644d54716c1a03a056c99f160a6bb13520
Vinland67 <13626@holbertonstudents.com>
2026-05-07 19:46:46 +0000

    Fix W391 and include PDF for Exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -16,6 +16,11 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            # lambda = 1 / mean
             self.lambtha = float(1 / (sum(data) / len(data)))
 
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given time period"""
+        if x < 0:
+            return 0
+        e = 2.7182818285
+        return self.lambtha * (e ** (-self.lambtha * x))
