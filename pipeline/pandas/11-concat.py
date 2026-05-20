dfb0c252fd9e8ca5d98dbaa4e675cb846db92b99
Vinland67 <13626@holbertonstudents.com>
2026-05-20 20:04:20 +0000

    Add 11-concat.py

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Modul iki DataFrame-i indeksləyib birləşdirmək üçün funksiya təmin edir.
+"""
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    Dataframe-ləri Timestamp üzrə indeksləyir və şərtə uyğun concat edir.
+    """
+    df1_indexed = index(df1)
+    df2_indexed = index(df2)
+
+    # 1417411920 daxil olmaqla df2-ni filtrləyirik
+    df2_filtered = df2_indexed.loc[:1417411920]
+
+    # df2-ni df1-in üzərinə keys (etiketlər) əlavə edərək birləşdiririk
+    return pd.concat([df2_filtered, df1_indexed], keys=['bitstamp', 'coinbase'])
