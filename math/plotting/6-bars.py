96a9d64e5fac53cc009ace920a678df749673d55
Vinland67 <13626@holbertonstudents.com>
2026-05-27 21:37:29 +0000

    Task 6: Implement unique stacked bar graph layout

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Generates a stacked bar chart for fruit distribution per person
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """
+    Plots stacked bars representing quantities of various fruits for individuals
+    """
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    people = ['Farrah', 'Fred', 'Felicia']
+    fruit_types = ['apples', 'bananas', 'oranges', 'peaches']
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    
+    w = 0.5
+    indices = np.arange(len(people))
+    current_bottom = np.zeros(len(people))
+
+    for idx in range(len(fruit_types)):
+        plt.bar(indices, fruit[idx], width=w, bottom=current_bottom,
+                color=colors[idx], label=fruit_types[idx])
+        current_bottom += fruit[idx]
+
+    plt.xticks(indices, people)
+    plt.yticks(np.arange(0, 81, 10))
+    plt.ylim(0, 80)
+    
+    plt.ylabel('Quantity of Fruit')
+    plt.title('Number of Fruit per Person')
+    plt.legend(loc='upper right')
+    
+    plt.show()
