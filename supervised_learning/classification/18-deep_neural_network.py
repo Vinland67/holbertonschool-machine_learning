76a8ce6940e69284a2341effcaa9e46db32972c3
Vinland67 <13626@holbertonstudents.com>
2026-07-08 20:17:47 +0000

    Add forward propagation to DeepNeuralNetwork

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -14,16 +14,19 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(n, int) and n > 0 for n in layers):
+        
+        types = set(map(type, layers))
+        if types != {int} or min(layers) <= 0:
             raise TypeError("layers must be a list of positive integers")
+
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
         prev = nx
         for l in range(1, self.__L + 1):
             self.__weights["W" + str(l)] = (
-                np.random.randn(layers[l - 1], prev)
-                * np.sqrt(2 / prev)
+                np.random.randn(layers[l - 1], prev) *
+                np.sqrt(2 / prev)
             )
             self.__weights["b" + str(l)] = np.zeros((layers[l - 1], 1))
             prev = layers[l - 1]
