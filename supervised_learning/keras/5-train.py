22709793ade09e2c1cdc2bcf9ff5872bedb34143
Vinland67 <13626@holbertonstudents.com>
2026-07-16 20:44:55 +0000

    Add Task 5 - train_model with validation

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module to train a Keras model with validation data
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None,
+                verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent and analyzes validation data
+    """
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    return history
