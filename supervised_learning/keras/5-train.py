04eebeac5f7d8720de1d0de18af5869eb6d220c2
Vinland67 <13626@holbertonstudents.com>
2026-07-16 20:46:52 +0000

    Fix PEP8 line length for Task 5

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -5,10 +5,10 @@ Module to train a Keras model with validation data
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
     """
-    Trains a model using mini-batch gradient descent and analyzes validation data
+    Trains a model and analyzes validation data
     """
     history = network.fit(
         x=data,
