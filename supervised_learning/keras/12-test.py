5c4b90c9280d7bca3df5c207d7befd0b837dfab6
Vinland67 <13626@holbertonstudents.com>
2026-07-17 19:59:13 +0000

    12. Test

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module to test a neural network
+"""
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Tests a neural network
+
+    network: the network model to test
+    data: the input data to test the model with
+    labels: the correct one-hot labels of data
+    verbose: boolean that determines if output should be printed
+        during the testing process
+
+    Returns: the loss and accuracy of the model with the testing
+        data, respectively
+    """
+    return network.evaluate(data, labels, verbose=verbose)
