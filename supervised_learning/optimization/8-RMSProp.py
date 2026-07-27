9d2c1978e163821197183b16959db23971d5b969
Vinland67 <13626@holbertonstudents.com>
2026-07-27 18:14:35 +0000

    8. fix pycodestyle

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -15,7 +15,6 @@ def create_RMSProp_op(alpha, beta2, epsilon):
 
     Returns: optimizer
     """
-    optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha,
-                                             rho=beta2,
-                                             epsilon=epsilon)
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha, rho=beta2, epsilon=epsilon)
     return optimizer
