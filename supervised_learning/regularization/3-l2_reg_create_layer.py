16cf75f2aa48bde89e289c59dc43416b470a7ad0
Vinland67 <13626@holbertonstudents.com>
2026-07-27 18:38:41 +0000

    3. add bias regularizer

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -23,5 +23,6 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     layer = tf.keras.layers.Dense(units=n,
                                    activation=activation,
                                    kernel_initializer=init,
-                                   kernel_regularizer=regularizer)
+                                   kernel_regularizer=regularizer,
+                                   bias_regularizer=regularizer)
     return layer(prev)
