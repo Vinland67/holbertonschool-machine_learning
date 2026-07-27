fdc28d4e77c09977f5ef6736fe5f33f62d2d1e87
Vinland67 <13626@holbertonstudents.com>
2026-07-27 18:43:02 +0000

    6. Create a Layer with Dropout - fix initializer scale

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -17,13 +17,16 @@ def dropout_create_layer(prev, n, activation, keep_prob, training=True):
 
     Returns: the output of the new layer
     """
-    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    dense = tf.keras.layers.Dense(units=n,
-                                   activation=activation,
-                                   kernel_initializer=init)
+    init = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
+    dense = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=init
+    )
     dropout = tf.keras.layers.Dropout(rate=1 - keep_prob)
 
     output = dense(prev)
-    output = dropout(output, training=training)
-
-    return output
+    return dropout(output, training=training)
