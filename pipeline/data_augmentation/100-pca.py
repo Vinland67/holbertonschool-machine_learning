0deb09b1bdbcd2294beac54130f1897b05e7ad79
Vinland67 <13626@holbertonstudents.com>
2026-07-27 19:32:55 +0000

    Fix PCA color augmentation return type and shapes

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -15,34 +15,25 @@ def pca_color(image, alphas):
 
     Returns: the augmented image
     """
-    # Flattens the image pixels to shape (N, 3) where N = height * width
-    orig_dtype = image.dtype
     img_float = tf.cast(image, dtype=tf.float32)
-    pixels = tf.reshape(img_float, (-1, 3))
+    reshaped_img = tf.reshape(img_float, (-1, 3))
 
-    # Mean center the pixels
-    mean = tf.reduce_mean(pixels, axis=0)
-    centered_pixels = pixels - mean
+    # Mean centering
+    mean = tf.reduce_mean(reshaped_img, axis=0)
+    centered = reshaped_img - mean
 
-    # Compute covariance matrix (3, 3)
-    num_pixels = tf.cast(tf.shape(pixels)[0], tf.float32)
-    cov = tf.matmul(centered_pixels, centered_pixels, transpose_a=True)
-    cov = cov / (num_pixels - 1)
+    # Covariance matrix calculation
+    cov = tf.matmul(centered, centered, transpose_a=True)
+    cov = cov / tf.cast(tf.shape(reshaped_img)[0] - 1, tf.float32)
 
-    # Compute eigenvalues and eigenvectors
-    eigenvalues, eigenvectors = tf.linalg.eigh(cov)
+    # Singular Value Decomposition (SVD) or Eigen decomposition
+    e_val, e_vec = tf.linalg.eigh(cov)
 
-    # Calculate delta offset: [p1, p2, p3] * ([alpha1, alpha2, alpha3] * [lambda1, lambda2, lambda3])
+    # Compute perturbation offset: e_vec * (alphas * e_val)
     alphas = tf.cast(alphas, dtype=tf.float32)
-    delta = tf.matmul(
-        eigenvectors,
-        tf.reshape(alphas * eigenvalues, (3, 1))
-    )
-
-    # Reshape delta to (1, 1, 3) and add to image
+    delta = tf.matmul(e_vec, tf.reshape(alphas * e_val, (3, 1)))
     delta = tf.reshape(delta, (1, 1, 3))
-    augmented_image = img_float + delta
 
-    # Clip values to valid range [0, 255] and restore original dtype
-    augmented_image = tf.clip_by_value(augmented_image, 0, 255)
-    return tf.cast(augmented_image, dtype=orig_dtype)
+    # Apply perturbation, clip to [0, 255], convert to uint8
+    augmented = tf.clip_by_value(img_float + delta, 0, 255)
+    return tf.cast(augmented, dtype=tf.uint8)
