01525f435d141597cefd3254427c0211fbe9d7f1
Vinland67 <13626@holbertonstudents.com>
2026-07-27 18:14:58 +0000

    10. fix pycodestyle

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -16,8 +16,6 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
 
     Returns: optimizer
     """
-    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha,
-                                          beta_1=beta1,
-                                          beta_2=beta2,
-                                          epsilon=epsilon)
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon)
     return optimizer
