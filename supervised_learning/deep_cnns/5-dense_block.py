0966635fd4a6ea9c37f74c3585b1d47026830749
Vinland67 <13626@holbertonstudents.com>
2026-08-16 11:07:03 +0000

    Implement dense block for DenseNet

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Module to build a dense block as described in
+Densely Connected Convolutional Networks
+"""
+from tensorflow import keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """
+    Builds a dense block as described in Densely Connected
+    Convolutional Networks
+
+    Args:
+        X: output from the previous layer
+        nb_filters: integer representing the number of filters in X
+        growth_rate: growth rate for the dense block
+        layers: number of layers in the dense block
+
+    Returns:
+        The concatenated output of each layer within the Dense Block
+        and the number of filters within the concatenated outputs
+    """
+    init = K.initializers.HeNormal(seed=0)
+
+    for i in range(layers):
+        # Bottleneck layer (DenseNet-B): BN -> ReLU -> 1x1 Conv (4 * growth_rate)
+        bn1 = K.layers.BatchNormalization(axis=3)(X)
+        act1 = K.layers.Activation('relu')(bn1)
+        conv1 = K.layers.Conv2D(
+            filters=4 * growth_rate,
+            kernel_size=(1, 1),
+            padding='same',
+            kernel_initializer=init
+        )(act1)
+
+        # Standard 3x3 Conv layer: BN -> ReLU -> 3x3 Conv (growth_rate)
+        bn2 = K.layers.BatchNormalization(axis=3)(conv1)
+        act2 = K.layers.Activation('relu')(bn2)
+        conv2 = K.layers.Conv2D(
+            filters=growth_rate,
+            kernel_size=(3, 3),
+            padding='same',
+            kernel_initializer=init
+        )(act2)
+
+        # Concatenate input with the output of current layer
+        X = K.layers.Concatenate(axis=3)([X, conv2])
+        nb_filters += growth_rate
+
+    return X, nb_filters
