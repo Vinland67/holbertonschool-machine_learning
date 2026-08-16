6e23972805f16bdcfca24d72d1cb25e1e3c885e2
Vinland67 <13626@holbertonstudents.com>
2026-08-16 10:52:11 +0000

    2. fix pycodestyle

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -28,24 +28,21 @@ def identity_block(A_prev, filters):
     F11, F3, F12 = filters
     init = K.initializers.HeNormal(seed=0)
 
-    conv1 = K.layers.Conv2D(filters=F11,
-                             kernel_size=(1, 1),
-                             padding='same',
-                             kernel_initializer=init)(A_prev)
+    conv1 = K.layers.Conv2D(
+        filters=F11, kernel_size=(1, 1), padding='same',
+        kernel_initializer=init)(A_prev)
     bn1 = K.layers.BatchNormalization(axis=3)(conv1)
     act1 = K.layers.Activation('relu')(bn1)
 
-    conv2 = K.layers.Conv2D(filters=F3,
-                             kernel_size=(3, 3),
-                             padding='same',
-                             kernel_initializer=init)(act1)
+    conv2 = K.layers.Conv2D(
+        filters=F3, kernel_size=(3, 3), padding='same',
+        kernel_initializer=init)(act1)
     bn2 = K.layers.BatchNormalization(axis=3)(conv2)
     act2 = K.layers.Activation('relu')(bn2)
 
-    conv3 = K.layers.Conv2D(filters=F12,
-                             kernel_size=(1, 1),
-                             padding='same',
-                             kernel_initializer=init)(act2)
+    conv3 = K.layers.Conv2D(
+        filters=F12, kernel_size=(1, 1), padding='same',
+        kernel_initializer=init)(act2)
     bn3 = K.layers.BatchNormalization(axis=3)(conv3)
 
     add = K.layers.Add()([bn3, A_prev])
