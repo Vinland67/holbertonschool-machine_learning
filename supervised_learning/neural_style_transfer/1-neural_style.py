3fa981b48414f71ab072ca2b0edcb3f9099d4434
Vinland67 <13626@holbertonstudents.com>
2026-08-21 15:06:44 +0000

    Add load_model method to NST class

---
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+"""
+Module defining the NST class for Neural Style Transfer
+"""
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """
+    Class that performs tasks for Neural Style Transfer
+    """
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        """
+        Class constructor for NST
+
+        Args:
+            style_image: numpy.ndarray of shape (h, w, 3)
+            content_image: numpy.ndarray of shape (h, w, 3)
+            alpha: weight for content cost
+            beta: weight for style cost
+        """
+        if not isinstance(style_image, np.ndarray) or \
+           style_image.ndim != 3 or style_image.shape[2] != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        if not isinstance(content_image, np.ndarray) or \
+           content_image.ndim != 3 or content_image.shape[2] != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        if not isinstance(alpha, (int, float)) or isinstance(alpha, bool) \
+           or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+
+        if not isinstance(beta, (int, float)) or isinstance(beta, bool) \
+           or beta < 0:
+            raise TypeError("beta must be a non-negative number")
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+        self.load_model()
+
+    @staticmethod
+    def scale_image(image):
+        """
+        Rescales an image such that its pixels values are between 0 and 1
+        and its largest side is 512 pixels
+
+        Args:
+            image: numpy.ndarray of shape (h, w, 3)
+
+        Returns:
+            scaled image as tf.Tensor
+        """
+        if not isinstance(image, np.ndarray) or \
+           image.ndim != 3 or image.shape[2] != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        h, w, _ = image.shape
+        if h > w:
+            h_new = 512
+            w_new = int(round((w * 512) / h))
+        else:
+            w_new = 512
+            h_new = int(round((h * 512) / w))
+
+        image_expanded = tf.expand_dims(image, axis=0)
+        resized_image = tf.image.resize(
+            image_expanded,
+            size=[h_new, w_new],
+            method=tf.image.ResizeMethod.BICUBIC
+        )
+
+        rescaled_image = resized_image / 255.0
+        rescaled_image = tf.clip_by_value(rescaled_image, 0.0, 1.0)
+
+        return rescaled_image
+
+    def load_model(self):
+        """
+        Creates the model used to calculate cost using VGG19 as a base.
+        Replaces MaxPooling2D layers with AveragePooling2D layers.
+        Saves model to self.model.
+        """
+        vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+        vgg.trainable = False
+
+        # MaxPool layihələrini AveragePooling2D ilə əvəz etmək üçün xüsusi struktur
+        custom_outputs = []
+        
+        # Bütün istənilən layerlərin siyahısı
+        target_layers = self.style_layers + [self.content_layer]
+
+        # VGG19 modelini custom Input ilə yenidən qururuq ki, AveragePooling istifadə olunsun
+        x = vgg.input
+        outputs = []
+
+        model_dict = {vgg.input.name: vgg.input}
+        
+        for layer in vgg.layers[1:]:
+            if isinstance(layer, tf.keras.layers.MaxPooling2D):
+                x = tf.keras.layers.AveragePooling2D(
+                    pool_size=layer.pool_size,
+                    strides=layer.strides,
+                    padding=layer.padding,
+                    name=layer.name
+                )(x)
+            else:
+                x = layer(x)
+
+            if layer.name in target_layers:
+                outputs.append(x)
+
+        model = tf.keras.models.Model(inputs=vgg.input, outputs=outputs)
+        model.trainable = False
+
+        self.model = model
