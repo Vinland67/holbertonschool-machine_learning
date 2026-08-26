8d9fd3abc73703d64fed76ce56bfa6970619f9ec
Vinland67 <13626@holbertonstudents.com>
2026-08-26 21:39:51 +0000

    Fix compile loss implementation using add_loss for VAE

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -41,6 +41,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         return z_mean + keras.backend.exp(z_log_sig / 2) * epsilon
 
     z = keras.layers.Lambda(sampling)([mean, log_sig])
+
     encoder = keras.Model(inputs=inputs, outputs=[z, mean, log_sig])
 
     # Decoder
@@ -49,30 +50,25 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     for nodes in reversed(hidden_layers):
         x = keras.layers.Dense(nodes, activation='relu')(x)
     outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+
     decoder = keras.Model(inputs=decoder_inputs, outputs=outputs)
 
     # Full Autoencoder
-    auto_outputs = decoder(encoder(inputs)[0])
+    auto_outputs = decoder(z)
     auto = keras.Model(inputs=inputs, outputs=auto_outputs)
 
-    def vae_loss(inputs, auto_outputs):
-        """
-        Calculates Variational Autoencoder loss
-        """
-        reconstruction_loss = keras.backend.binary_crossentropy(
-            inputs, auto_outputs
-        )
-        reconstruction_loss = keras.backend.sum(
-            reconstruction_loss, axis=-1
-        )
+    # VAE Loss
+    reconstruction_loss = keras.losses.binary_crossentropy(inputs, auto_outputs)
+    reconstruction_loss *= input_dims
 
-        kl_loss = 1 + log_sig - keras.backend.square(mean) - \
-            keras.backend.exp(log_sig)
-        kl_loss = keras.backend.sum(kl_loss, axis=-1)
-        kl_loss *= -0.5
+    kl_loss = 1 + log_sig - keras.backend.square(mean) - \
+        keras.backend.exp(log_sig)
+    kl_loss = keras.backend.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
 
-        return keras.backend.mean(reconstruction_loss + kl_loss)
+    vae_loss = keras.backend.mean(reconstruction_loss + kl_loss)
+    auto.add_loss(vae_loss)
 
-    auto.compile(optimizer='adam', loss=vae_loss)
+    auto.compile(optimizer='adam')
 
     return encoder, decoder, auto
