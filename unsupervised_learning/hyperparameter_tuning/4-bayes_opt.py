d598877b4d44974d37355c3e668a124fe13f5dfd
Vinland67 <13626@holbertonstudents.com>
2026-08-26 21:05:51 +0000

    Implement acquisition method using Expected Improvement

---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Module defining the BayesianOptimization class with acquisition method
+"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """
+    Class that performs Bayesian optimization on a
+    noiseless 1D Gaussian process
+    """
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """
+        Class constructor for BayesianOptimization
+
+        Args:
+            f: black-box function to be optimized
+            X_init: numpy.ndarray of shape (t, 1) representing initial inputs
+            Y_init: numpy.ndarray of shape (t, 1) representing initial outputs
+            bounds: tuple of (min, max) representing search space bounds
+            ac_samples: number of samples to analyze during acquisition
+            l: length parameter for the kernel
+            sigma_f: standard deviation for output
+            xsi: exploration-exploitation factor for acquisition
+            minimize: bool determining minimization (True) or
+                      maximization (False)
+        """
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        min_bound, max_bound = bounds
+        self.X_s = np.linspace(min_bound, max_bound, ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """
+        Calculates the next best sample location using Expected Improvement
+
+        Returns:
+            X_next: numpy.ndarray of shape (1,) representing next sample point
+            EI: numpy.ndarray of shape (ac_samples,) containing expected
+                improvement of each potential sample
+        """
+        mu, sigma = self.gp.predict(self.X_s)
+
+        if self.minimize:
+            y_opt = np.min(self.gp.Y)
+            improvement = y_opt - mu - self.xsi
+        else:
+            y_opt = np.max(self.gp.Y)
+            improvement = mu - y_opt - self.xsi
+
+        with np.errstate(divide='ignore'):
+            Z = improvement / sigma
+            ei = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[sigma == 0.0] = 0.0
+
+        X_next = self.X_s[np.argmax(ei)]
+
+        return X_next, ei
