3fac52564cb352c968fe296f6668d4e45752160f
Vinland67 <13626@holbertonstudents.com>
2026-09-07 15:00:13 +0000

    Implement bag_of_words function in supervised_learning/word_embeddings

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Module defining the bag_of_words function
+"""
+import numpy as np
+import re
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words embedding matrix
+
+    Args:
+        sentences: list of sentences to analyze
+        vocab: list of the vocabulary words to use, or None
+
+    Returns:
+        embeddings: numpy.ndarray of shape (s, f) containing the embeddings
+        features: list of the features used for embeddings
+    """
+    cleaned_sentences = []
+    for sentence in sentences:
+        words = re.findall(r'\b[a-zA-Z0-9]+\b', sentence.lower())
+        cleaned_sentences.append(words)
+
+    if vocab is None:
+        vocab_set = set()
+        for words in cleaned_sentences:
+            for word in words:
+                vocab_set.add(word)
+        features = sorted(list(vocab_set))
+    else:
+        features = sorted(list(set(vocab)))
+
+    embeddings = np.zeros((len(sentences), len(features)), dtype=int)
+
+    for i, words in enumerate(cleaned_sentences):
+        for word in words:
+            if word in features:
+                j = features.index(word)
+                embeddings[i, j] += 1
+
+    return embeddings, features
---
+++ supervised_learning/word_embeddings/README.md
@@ -0,0 +1,3 @@
+# Word Embeddings
+
+This directory contains implementations of word embedding techniques such as Bag of Words, TF-IDF, Word2Vec, GloVe, and FastText.
