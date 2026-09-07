c343847d0da62d707df7dc6b3cb2a429f47f1975
Vinland67 <13626@holbertonstudents.com>
2026-09-07 16:14:53 +0000

    Fix line length completely in 4-positional_encoding.py

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -14,20 +14,19 @@ def positional_encoding(max_seq_len, dm):
         dm: model depth
 
     Returns:
-        numpy.ndarray of shape (max_seq_len, dm) containing positional encodings
+        numpy.ndarray of shape (max_seq_len, dm)
+        containing positional encodings
     """
     PE = np.zeros((max_seq_len, dm))
     pos = np.arange(max_seq_len)[:, np.newaxis]
     i = np.arange(dm)[np.newaxis, :]
 
-    angle_rates = pos / np.power(
+    div_term = np.power(
         10000, (2 * (i // 2)) / np.float32(dm)
     )
+    angle_rates = pos / div_term
 
-    # Apply sin to even indices in the array; 2i
     PE[:, 0::2] = np.sin(angle_rates[:, 0::2])
-
-    # Apply cos to odd indices in the array; 2i+1
     PE[:, 1::2] = np.cos(angle_rates[:, 1::2])
 
     return PE
