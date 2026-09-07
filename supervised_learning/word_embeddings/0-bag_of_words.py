fe563ae318277ad0d83bd8a0733eb45520221a26
Vinland67 <13626@holbertonstudents.com>
2026-09-07 15:34:11 +0000

    Clean whitespace and update bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python3
 """
-Module defining the bag_of_words function
+Creates a bag of words embedding matrix
 """
+import re
 import numpy as np
 
 
 def bag_of_words(sentences, vocab=None):
     """
-    Creates a bag of words embedding matrix
+    Creates a bag of words embedding matrix.
 
     Args:
         sentences: list of sentences to analyze
-        vocab: list of the vocabulary words to use, or None
+        vocab: list of the vocabulary words to use for the analysis.
+               If None, all words within sentences should be used.
 
     Returns:
         embeddings: numpy.ndarray of shape (s, f) containing the embeddings
-        features: list of the features used for embeddings
+        features: numpy.ndarray of the features used for embeddings
     """
-    cleaned_sentences = []
+    sents_words = []
     for sentence in sentences:
-        # Aşağıdakı təmizləmə apostrofları və xüsusi simvolları nəzərə alır
-        words = []
-        for word in sentence.lower().split():
-            # Təmiz söz əldə etmək üçün hərflər və rəqəmlər saxlanılır
-            cleaned = "".join(c for c in word if c.isalnum())
-            if cleaned:
-                words.append(cleaned)
-        cleaned_sentences.append(words)
+        s = sentence.lower()
+        s = re.sub(r"'s\b", "", s)
+        words = re.findall(r'\b\w+\b', s)
+        sents_words.append(words)
 
     if vocab is None:
-        vocab_set = set()
-        for words in cleaned_sentences:
-            for word in words:
-                vocab_set.add(word)
-        features = sorted(list(vocab_set))
-    else:
-        features = sorted(list(set(vocab)))
+        unique_words = set()
+        for words in sents_words:
+            unique_words.update(words)
+        vocab = sorted(list(unique_words))
 
-    embeddings = np.zeros((len(sentences), len(features)), dtype=int)
+    features = np.array(vocab)
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+    vocab_dict = {w: i for i, w in enumerate(vocab)}
 
-    for i, words in enumerate(cleaned_sentences):
+    for i, words in enumerate(sents_words):
         for word in words:
-            if word in features:
-                j = features.index(word)
-                embeddings[i, j] += 1
+            if word in vocab_dict:
+                embeddings[i, vocab_dict[word]] += 1
 
     return embeddings, features
